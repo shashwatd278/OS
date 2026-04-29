3bd8a234a6a5ec9bf1488534ec6d2055e36b7ad0
shashwatd278 <shashwatd.cs24@bmsce.ac.in>
2026-04-29 11:55:49 +0530

    Add files via upload

---
+++ Rate_Monotomic/ratemono.c
@@ -0,0 +1,114 @@
+#include<stdio.h>
+#include<math.h>
+
+struct tasks {
+    int pid;
+    int Ci;
+    int Ti;
+    int remaining;
+    int next_arrival;
+};
+
+int gcd(int a, int b) {
+    while(b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int lcm(int a,int b) {
+    return (a*b)/gcd(a,b);
+}
+
+int tasks_lcm(struct tasks t[],int n) {
+    int result=t[0].Ti;
+
+    for(int i=1;i<n;i++) {
+        result=lcm(result,t[i].Ti);
+    }
+    return result;
+}
+
+int main() {
+
+    int n;
+    printf("enter number of tasks: ");
+    scanf("%d",&n);
+
+    struct tasks t[n];
+
+    for(int i=0;i<n;i++) {
+        printf("Task %d\n", i+1);
+
+        printf("enter Ci: ");
+        scanf("%d", &t[i].Ci);
+
+        printf("enter Ti: ");
+        scanf("%d", &t[i].Ti);
+
+        t[i].pid = i+1;
+
+        // init for step 5
+        t[i].remaining = 0;
+        t[i].next_arrival = 0;
+    }
+
+    float u=0;
+    for(int i=0;i<n;i++) {
+        u += (float)t[i].Ci / t[i].Ti;
+    }
+    printf("CPU utilization: %f\n", u);
+
+    float bound = n * (pow(2, (float)1/n) - 1);
+    printf("Bound: %f\n", bound);
+
+    if(u <= bound) {
+        printf("Schedulable using RMS\n");
+    } else {
+        printf("Not guaranteed schedulable\n");
+    }
+
+    for(int i=0;i<n;i++) {
+        for(int j=i+1;j<n;j++) {
+            if(t[i].Ti > t[j].Ti) {
+                struct tasks temp = t[i];
+                t[i] = t[j];
+                t[j] = temp;
+            }
+        }
+    }
+
+
+    int LIMIT = tasks_lcm(t,n);
+
+    printf("\n--- Scheduling ---\n");
+
+    for(int time = 0; time < LIMIT; time++) {
+
+        for(int i=0;i<n;i++) {
+            if(time == t[i].next_arrival) {
+                t[i].remaining = t[i].Ci;
+                t[i].next_arrival += t[i].Ti;
+            }
+        }
+
+        int idx = -1;
+        for(int i=0;i<n;i++) {
+            if(t[i].remaining > 0) {
+                idx = i;
+                break;
+            }
+        }
+
+        if(idx != -1) {
+            printf("Time %d: Task %d\n", time, t[idx].pid);
+            t[idx].remaining--;
+        } else {
+            printf("Time %d: Idle\n", time);
+        }
+    }
+
+    return 0;
+}
